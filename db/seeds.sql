c36e05a7eae1e465887203a4fd3d816da8f26dfe
anthnsm <anthony.smith353@gmail.com>
2020-07-02 19:46:46 -0700

    Uploading package.json

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES("Philly Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES("Black N Blue Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES("Vegan Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES("Double Bacon Cheeseburger");
